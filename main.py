1424aec579b1116fc704bbfee6d411d69a5304e0
Nachiket07R <136220514+Nachiket07R@users.noreply.github.com>
2023-07-02 01:16:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import time
 import os
 import threading
 
-bot_token = os.environ.get("TOKEN", "") 
-api_hash = os.environ.get("HASH", "") 
-api_id = os.environ.get("ID", "")
-ss = os.environ.get("STRING", "")
+bot_token = os.environ.get("TOKEN", "6271783125:AAE56OlOLWsYaUJ72EzbtUZccXa9IOlaSkY") 
+api_hash = os.environ.get("HASH", "2b33c04f7e8c03a7ce2591cb9613ab91") 
+api_id = os.environ.get("ID", "21374968")
+ss = os.environ.get("STRING", "BQC0N3XBqqrlHyhnO_9CQG7u8dZ9WGk_SDwkt5dhZWaBtA4iRvZ6m_GI5cRENLOIG1odJVJmHQD_9pT_vvYUuXSoZqagIS03FsalRKCOABLCqq8_KJ1n_NO_1xyhzoJEXC_5Bl9Tn1isZ7IhpZRwlFqfONxYrTqMIk7y68PeAZeEtsML6bO1WnsQs8KN18UoGzfA2yMpjhuHK79r0Ke5erpf26OPYYrAfiWylNvvlM09n23lwjs1pyeEHEGhw-ESkahXyvekisApkflIhdUYzEE46k8Oix9qD_0zJS4R7gVCXeeahE2U0arVvqEKRAxAR6rsmBH6GHKYzbt9mTczKzbGS1R1qQA")
 bot = Client("mybot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
 acc = Client("myacc",api_id=api_id,api_hash=api_hash,session_string=ss)
 
